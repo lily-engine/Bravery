a124836043fdd241f09a41b5868a41c9e16e728e
kikuchi <nyoronyoro0327@gmail.com>
2022-03-27 02:58:29 +0900

    checkbox追加

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from app import app
 
 app = Flask(__name__)
 
@@ -7,11 +6,11 @@ app = Flask(__name__)
 def index():
     question_lists = ["HTML","CSS","JavaScript","JavaScriptのフレームワーク","バックエンドの言語","バックエンド言語のフレームワーク","Linux","Git/GitHub","Webの仕組み","MySQL/SQL","ポートフォリオ作成"]
     return render_template('view.html',
-        title = '目指せエンジニア！学習到達度チェッカー',
-        message = '学習済の項目にチェックを入れてください。',
+        title = '目指せバックエンドエンジニア！学習到達度チェッカー',
+        message = '学習済の項目にチェックを入れてください。学習の到達度を判定します！',
         question_lists = question_lists
     )
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
