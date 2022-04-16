0ad01e2ab71ad0e75bb64380dba976fa5a760a5d
kikuchi <nyoronyoro0327@gmail.com>
2022-04-17 00:48:11 +0900

    tweet_url

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,6 +2,6 @@ document.getElementById("twitter-share-button").onclick = function() {
     let text = document.getElementById("tweet-text").innerText;
     let message = "あなたの勉強の進捗は" + text + "％です！今日も楽しく勉強しよう🐥"
     let hashtags = "ぴよぴよエンジニア";
-    let url = encodeURIComponent(location.href)
+    let url = "https://piyopiyo-engineer.net/";
     window.open("https://twitter.com/share?text=" + message + "&hashtags=" + hashtags + "&url=" + url);
 }
